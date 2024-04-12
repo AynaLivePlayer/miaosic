4da760b51a87417b8852dc373c96417b8c1ed2bb
aynakeya <aynakeya.official@gmail.com>
2024-04-11 22:36:45 -0700

    fix playlist

--- providers/local/playlist.go
+++ providers/local/playlist.go
@@ -2,10 +2,10 @@ package local
 
 import "github.com/AynaLivePlayer/miaosic"
 
-func (l *localPlaylist) MatchPlaylist(uri string) (miaosic.MetaData, bool) {
+func (l *Local) MatchPlaylist(uri string) (miaosic.MetaData, bool) {
 	return miaosic.MetaData{}, false
 }
 
-func (l *localPlaylist) GetPlaylist(meta miaosic.MetaData) (*miaosic.Playlist, error) {
+func (l *Local) GetPlaylist(meta miaosic.MetaData) (*miaosic.Playlist, error) {
 	return nil, miaosic.ErrNotImplemented
 }
