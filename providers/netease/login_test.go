0dde02e588939e6032bf837b30ed4d1d79f1ddc9
aynakeya <aynakeya.official@gmail.com>
2025-07-24 18:51:05 +0800

    update test

--- providers/netease/login_test.go
+++ providers/netease/login_test.go
@@ -1,1 +1,16 @@
 package netease
+
+//func TestNetease_IsLogin(t *testing.T) {
+//	interfaces, err := net.Interfaces()
+//	if err != nil {
+//		fmt.Println("Error getting network interfaces:", err)
+//		return
+//	}
+//
+//	for _, iface := range interfaces {
+//		// Filter out loopback interfaces and interfaces without a hardware address
+//		if iface.Flags&net.FlagLoopback == 0 && iface.HardwareAddr != nil {
+//			fmt.Printf("Interface: %s, MAC Address: %s\n", iface.Name, iface.HardwareAddr.String())
+//		}
+//	}
+//}
