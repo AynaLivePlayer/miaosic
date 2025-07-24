765c5c0da7aff89eeb44f1b034e7df605c2c8177
aynakeya <aynakeya.official@gmail.com>
2025-07-24 18:48:46 +0800

    update test

--- providers/netease/playlist_test.go
+++ providers/netease/playlist_test.go
@@ -3,6 +3,7 @@ package netease
 import (
 	"github.com/AynaLivePlayer/miaosic"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -17,4 +18,11 @@ func TestGetPlaylist(t *testing.T) {
 	for _, media := range playlist.Medias {
 		t.Log(media.Title, media.Artist, media.Album)
 	}
+}
+
+func TestNetease_MatchPlaylist(t *testing.T) {
+	meta, ok := api.MatchPlaylist("https://music.163.com/#/playlist?id=3204776824")
+	assert.True(t, ok)
+	require.Equal(t, api.GetName(), meta.Provider)
+	require.Equal(t, "3204776824", meta.Identifier)
 }
